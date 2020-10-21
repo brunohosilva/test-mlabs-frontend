db96f5e6aebba05a061467a5e1b0756c4b4452a1
Bruno Oliveira <bruno.oliveira@climatempo.com.br>
2020-10-21 16:50:37 -0300

    :wrench: Included eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: '@react-native-community',
+  root: true,
+  rules: {
+    'no-console': 'error',
+    'no-undef': 'off',
+    'no-useless-escape': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'sort-keys': ['error'],
+  },
+};
