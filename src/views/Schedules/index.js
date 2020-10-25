12dc7bb21792263022e73f3c2b60186d574f0b6d
Bruno Oliveira <bruno.oliveira@climatempo.com.br>
2020-10-25 13:02:17 -0300

    :construction: Work in progress to make schedule list with dynamic json

--- src/views/Schedules/index.js
+++ src/views/Schedules/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 import Header from '../../components/Header';
-
+import schedulesList from '../../assets/json/schedules';
 import Image from '../../lib/Images';
 import './style.scss';
 
 const Schedules = () => {
   const data = localStorage.getItem('data');
   const response = JSON.parse(data);
+  const list = schedulesList.data;
+  const {Instagram, Linkedin} = Image;
 
   return (
     <div className="container">
@@ -41,7 +44,72 @@ const Schedules = () => {
                     {item.date} ás {item.hour}h
                   </td>
                   <td>preview</td>
-                  <td className="description">Agendado</td>
+                  <td className="status">
+                    <div
+                      className="typeStatus"
+                      style={{backgroundColor: '#2F80ED'}}
+                    />
+                    <span className="description">Agendado</span>
+                  </td>
+                </tr>
+              );
+            })}
+            {list.map((item, index) => {
+              const date = moment(item.publication_date).format('DD/MM/YY');
+              const hour = moment(item.publication_date).format('HH:mm');
+
+              let logo;
+              var aStr = item.social_network_key;
+              for (var i = 0; i < aStr.length; i++) {
+                var supp = aStr[i];
+                switch (supp) {
+                  case 2 && 3:
+                    logo = `<img className="tesssteeeee" src=${Instagram} />  <img className="tesssteeeee" src=${Linkedin} />`;
+                    break;
+
+                  case 2:
+                    logo = `<img className="tesssteeeee" src=${Instagram} />`;
+                    break;
+                }
+              }
+
+              let status;
+              let statusColor;
+              switch (item.status_key) {
+                case 1:
+                  status = 'Postado';
+                  statusColor = '#27AE60';
+                  break;
+                case 3:
+                  status = 'Postado com ressalvas';
+                  statusColor = '#F2C94C';
+                  break;
+              }
+              return (
+                <tr key={index}>
+                  <td
+                    dangerouslySetInnerHTML={{__html: logo}}
+                    className="socialLogoContainer"
+                  />
+                  <td className="heigth">
+                    <img className="imagePostContainer" src={item.media} />
+                  </td>
+                  <td>
+                    <p className="description">{item.text}</p>
+                  </td>
+                  <td>
+                    <span className="description">
+                      {date} ás {hour}h
+                    </span>
+                  </td>
+                  <td>preview</td>
+                  <td className="status">
+                    <div
+                      className="typeStatus"
+                      style={{backgroundColor: statusColor}}
+                    />
+                    <span className="description">{status}</span>
+                  </td>
                 </tr>
               );
             })}
