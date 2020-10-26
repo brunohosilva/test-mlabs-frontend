49423bf8fc12d842c9c11463df621e7afb4afece
Bruno Oliveira <bruno.oliveira@climatempo.com.br>
2020-10-26 17:13:44 -0300

    :lipstick: Included select animation when choose social networking

--- src/components/SocialNetwork/index.js
+++ src/components/SocialNetwork/index.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react';
 import Image from '../../lib/Images';
 import Social from '../../assets/json/social-networks';
+
 import './style.scss';
 
 const SocialNetwork = (props) => {
   const {SetSocial = () => {}} = props;
   const [socialSelected, setSocialSelection] = useState('');
+  const [isSelected, setIsSelection] = useState(false);
+  const [indexSelected, setIndexSelection] = useState('');
+
+  function Selected(name, index) {
+    setIndexSelection(index);
+    setIsSelection(!isSelected);
+    setSocialSelection(name);
+  }
   SetSocial(socialSelected);
 
   const data = Social.data;
@@ -17,15 +26,25 @@ const SocialNetwork = (props) => {
       <div className="socialList">
         {data.map((item, index) => {
           const icon = Image[item.name];
-          const status = item.status === 'disabled' ? 'block' : 'pointer';
+
+          let backgroundColorSocial;
+          if (item.status === 'disabled') {
+            backgroundColorSocial = '#DDDDDD';
+          } else if (isSelected && indexSelected === index) {
+            backgroundColorSocial = '#6effab';
+          }
+
           return (
             <div
-              className={status}
+              style={{
+                backgroundColor: backgroundColorSocial,
+              }}
+              className="circle"
               key={index}
               onClick={() =>
                 item.status === 'disabled'
-                  ? console.log('notificar que está bloqueado')
-                  : setSocialSelection(item.name)
+                  ? console.log('rede social bloqueada')
+                  : Selected(item.name, index)
               }>
               <img className="socialLogo" src={icon} alt={item.name} />
             </div>
